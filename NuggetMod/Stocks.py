9517a0d13ead05bdb0e2a0de0d2193d3535327e4
Dawson <djtorch123@gmail.com>
2020-07-18 19:35:53 -0400

    Added Disney Stock

--- NuggetMod/Stocks.py
+++ NuggetMod/Stocks.py
@@ -7,68 +7,28 @@ Created on Mon Jul 13 23:12:50 2020
 
 import robin_stocks as r
 import os
-import FileManager as fmanager ####ADD 'from .'
+from . import FileManager as fmanager ####ADD 'from .'
 
 
 r.login(str(os.getenv('GMAIL_USERNAME')), str(os.getenv('ROBINHOOD_PASSWORD')))
 
+#ACCOUNT VALUE
+accountValue = str(r.profiles.load_portfolio_profile('market_value'))
+fAccount = float(accountValue)    
+Account = str(round(fAccount,2))
+
+#STOCKS
+gluu = str(r.stocks.get_quotes('GLUU', 'ask_price'))
+ual = str(r.stocks.get_quotes('UAL', 'ask_price'))
+gpro = str(r.stocks.get_quotes('GPRO','ask_price'))
+acb = str(r.stocks.get_quotes('ACB','ask_price'))
+jets = str(r.stocks.get_quotes('JETS','ask_price'))
+dis = str(r.stocks.get_quotes('DIS', 'ask_price'))
 
 
-
-def getStockInfo():
-
-    accountValue = str(r.profiles.load_portfolio_profile('market_value'))
-    fAccount = float(accountValue)    
-    Account = str(round(fAccount,2))
-    
-    gluu = str(r.stocks.get_quotes('GLUU', 'ask_price'))
-    ual = str(r.stocks.get_quotes('UAL', 'ask_price'))
-    gpro = str(r.stocks.get_quotes('GPRO','ask_price'))
-    acb = str(r.stocks.get_quotes('ACB','ask_price'))
-    jets = str(r.stocks.get_quotes('JETS','ask_price'))
-    
-    GLUU = parseOpenPrice(gluu)
-    UAL = parseOpenPrice(ual)
-    GPRO = parseOpenPrice(gpro)
-    ACB = parseOpenPrice(acb)
-    JETS = parseOpenPrice(jets)
-    
-    btc = str(r.crypto.get_crypto_quote('BTC', 'mark_price'))
-    fBTC = float(btc)
-    BTC = str(round(fBTC,2))
-    
-    doge = str(r.crypto.get_crypto_quote('DOGE', 'mark_price'))
-    fdoge = float(doge)
-    DOGE = str(round(fdoge,3))
-    
-    
-    print(GLUU)
-    print(UAL)
-    print(GPRO)
-    print(ACB)
-    print(BTC)
-    print(JETS)
-    print(Account)
-    
-
-    
-    stockBriefing = ("Here is your Stock Portfolio Update," +
-                     "\n" + f"Your Account's Total Value is {Account}," +
-                     "\n" + f"Glue Mobile Market Price is {GLUU}," +
-                     "\n" + f"United Airlines Market price is {UAL}," +
-                     "\n" + f"Global JETS E T F Market price is {JETS}," +
-                     "\n" + f"Go Pro Market Price is {GPRO}" +
-                     "\n" + f"Aurora Cannabis Market price is {ACB}" +
-                     "\n" +
-                     "\n" + "Here is your Crypto Update," +
-                     "\n" + f"Bit coin Market Price is {BTC}," +
-                     "\n" + f"Doge coin Market Price is {DOGE}" +
-                     "\n"
-                     )
-    
-    r.authentication.logout()
-    
-    return stockBriefing
+def setStockString(name, stockticker):
+    words = f"{name} Market Price is {stockticker},"
+    return words
 
 def parseOpenPrice(ticker):
     sub = ticker.replace('[', '')
@@ -78,26 +38,44 @@ def parseOpenPrice(ticker):
     roundedticker = str(round(fticker,2))
     return roundedticker
 
+#PARSING STOCKS FOR STRING VALUES
+GLUU = parseOpenPrice(gluu)
+UAL = parseOpenPrice(ual)
+GPRO = parseOpenPrice(gpro)
+ACB = parseOpenPrice(acb)
+JETS = parseOpenPrice(jets)
+DIS = parseOpenPrice(dis)
 
-def test():
-    #accountValue = str(r.profiles.load_portfolio_profile('market_value'))
-    #print(accountValue)
-    print(float(r.profiles.load_portfolio_profile('market_value'))+7.45)
+#CRYPTO CURRENCIES
+btc = str(r.crypto.get_crypto_quote('BTC', 'mark_price'))
+doge = str(r.crypto.get_crypto_quote('DOGE', 'mark_price'))
+
+#PARSING CRYPTO FOR STRING VALUES
+fBTC = float(btc)
+BTC = str(round(fBTC,2))
+
+fdoge = float(doge)
+DOGE = str(round(fdoge,3))
+
+
+def getStockInfo():
+    stockBriefing = ("Here is your Stock Portfolio Update," +
+                     "\n" + f"Your Account's Total Value is {Account}," +
+                     "\n" + setStockString('Glu Mobile', GLUU) +
+                     "\n" + setStockString('United Airlines', UAL) +
+                     "\n" + setStockString('Global Jets E T F', JETS) +
+                     "\n" + setStockString("Disney", DIS) +
+                     "\n" + setStockString('Go Pro', GPRO) +
+                     "\n" + setStockString('Aurora Cannibis', ACB) +
+                     "\n" +
+                     "\n" + "Here is your Crypto Update," +
+                     "\n" + setStockString('Bit Coin', BTC) +
+                     "\n" + setStockString('Doge Coin', DOGE) +
+                     "\n"
+                     )
     r.authentication.logout()
-    #print(getStockInfo())
+    print(stockBriefing)
+    return stockBriefing
+getStockInfo()
     
 
-#test()
-    
-#getStockInfo()
-
-
-'''
-Playground
-'''
-
-def setStockString(name, stockticker):
-    words = f"{name} Market Price is {GLUU},"
-    return words
-
-print(setStockString(Glu Mobile, GLUU))
